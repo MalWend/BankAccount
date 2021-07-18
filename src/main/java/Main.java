882709fcc542dbe6dd1ab5ef58e334b7e52d5b47
Your name <Magorzata abendowicz>
2021-07-18 22:27:04 +0200

    checked in Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount Super = new BankAccount(500);
+        Super.transfer(1000);
+        Super.withdraw(700);
+        Super.getAccountBalance();
+        Super.withdraw(20);
+    }
+}
