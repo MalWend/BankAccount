44d764c6b5417c7c777f3b6fc0b44336834c2108
Your name <Magorzata abendowicz>
2021-07-18 22:15:47 +0200

    created BankAccount

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -3,37 +3,38 @@ public class BankAccount {
 
     //Konstruktor bezparametrowy//
 
-    public BankAccount(){
+    public BankAccount() {
         this.accountBalance = 0;
     }
 
     //Konstruktor z jednym parametrem,
     // będziemy przypisywać wartość wprowadzanej  zmiennej i ustalać jej wartość jako globalną//
 
-    public BankAccount(int accountBalance){
+    public BankAccount(int accountBalance) {
         this.accountBalance = accountBalance;
     }
 
     //Metoda pierwsza -publiczna, zwracająca wartość logiczną przyjmująca
     // jedną zmienną typu liczbowego „amount”, nazwa metody „withdraw”
 
-    public boolean withdraw(int amount){
-        if (amount > this.accountBalance){
+    public boolean withdraw(int amount) {
+        if (amount > this.accountBalance) {
             return false;
-        } this.accountBalance -= amount;
+        }
+        this.accountBalance -= amount;
         return true;
     }
     //Metoda druga -publiczna, która nie zwraca żadnej wartości
     // przyjmująca jedną zmienną typu liczbowego „amount”, nazwa metody „transfer”
 
-    public void transfer(int amount){
+    public void transfer(int amount) {
         this.accountBalance += amount;
     }
 
     //Metoda trzecia - publiczna zwracająca wartość liczbową,
     // nie przyjmująca żadnej zmiennej,nazwa metody „getAccountBalance”
 
-    public int getAccountBalance(){
+    public int getAccountBalance() {
         return this.accountBalance;
     }
 }
